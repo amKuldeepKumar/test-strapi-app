fa19e00514dbb296306cb0050b530a0ece4dbf4c
kuldeep <kuldeep@insonix.com>
2025-09-04 16:51:06 +0530

    ..

--- config/database.js
+++ config/database.js
@@ -1,34 +1,19 @@
-const path = require("path");
-
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        ssl: env.bool("DATABASE_SSL", true) ? { rejectUnauthorized: false } : false,
-        schema: env("DATABASE_SCHEMA", "public"),
-        family: 4, // Force IPv4
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db")),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
-  };
-};
+    debug: false,
+  },
+});
+
+
