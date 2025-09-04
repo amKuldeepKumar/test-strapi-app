c4e5e3975aff23698ad75f73882e676887c26c41
kuldeep <kuldeep@insonix.com>
2025-09-04 16:54:15 +0530

    ..

--- config/database.js
+++ config/database.js
@@ -1,19 +1,11 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: 'postgres',
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: {
-        
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', false)
+        ? { rejectUnauthorized: false }
+        : false,
     },
-    debug: false,
   },
 });
-
-
